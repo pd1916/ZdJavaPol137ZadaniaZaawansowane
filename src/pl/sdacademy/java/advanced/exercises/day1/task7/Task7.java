a514f061555dd583b41ade42ad647d218167c47d
Pawel Dudko <paweldudko@outlook.com>
2023-03-04 13:00:50 +0100

    day1.task7 solution

---
+++ src/pl/sdacademy/java/advanced/exercises/day1/task7/Task7.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.java.advanced.exercises.day1.task7;
+
+public class Task7 {
+    public static void main(String[] args) {
+        WeaponClip weaponClip = new WeaponClip(3);
+        System.out.println("IsLoaded? " +weaponClip.isLoaded());
+        weaponClip.loadBullet("1");
+        weaponClip.loadBullet("2");
+        weaponClip.loadBullet("3");
+        //weaponClip.loadBullet("4");
+        System.out.println("IsLoaded? " +weaponClip.isLoaded());
+        System.out.println("***");
+        weaponClip.shot();
+        weaponClip.shot();
+        weaponClip.shot();
+        System.out.println("IsLoaded? " +weaponClip.isLoaded());
+        weaponClip.shot();
+    }
+}
---
+++ src/pl/sdacademy/java/advanced/exercises/day1/task7/WeaponClip.java
@@ -0,0 +1,38 @@
+package pl.sdacademy.java.advanced.exercises.day1.task7;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class WeaponClip {
+    // LIFO -> last input, first output -> np Stack, Deque
+    // FIFO -> first input, first output -> np Queue
+    //private final Stack<String> weaponClip = new Stack<>();
+    private final Deque<String> weaponClip = new ArrayDeque<>();
+    private final int maxNumberOfBulletInClip;
+
+    public WeaponClip(int clipCapacity) {
+        maxNumberOfBulletInClip = clipCapacity;
+    }
+
+    public void loadBullet(String bullet) {
+        if(maxNumberOfBulletInClip  == weaponClip.size()) {
+            throw new IllegalArgumentException("Clip is fill");
+        }
+        weaponClip.push(bullet);
+    }
+
+    public boolean isLoaded() {
+        return weaponClip.size() >= 1;
+        //return !weaponClip.isEmpty() // dla Deque
+        //return !weaponClip.empty(); // dla Stack
+    }
+
+    public void shot() {
+        if(isLoaded()) {
+            System.out.println(weaponClip.pop());
+        } else {
+            System.out.println("Clip is empty");
+        }
+    }
+}
