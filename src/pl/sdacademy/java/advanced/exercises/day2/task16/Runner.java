fd304710b02bddba313edcf1033f6f101337752c
Pawel Dudko <paweldudko@outlook.com>
2023-03-05 12:33:18 +0100

    solution for task 16

---
+++ src/pl/sdacademy/java/advanced/exercises/day2/task16/Runner.java
@@ -0,0 +1,28 @@
+package pl.sdacademy.java.advanced.exercises.day2.task16;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum Runner {
+    BEGINNER(1024, Integer.MAX_VALUE),
+    INTERMEDIATE(512, 1024),
+    ADVANCED(0, 512);
+
+    private final int minMinutes;
+    private final int maxMinutes;
+
+    private static final Runner[] VALUES = values();
+
+    Runner(int minMinutes, int maxMinutes) {
+        this.minMinutes = minMinutes;
+        this.maxMinutes = maxMinutes;
+    }
+
+    public static Runner getFitnessLevel(int runningTime) {
+        return Arrays.stream(VALUES)
+                .filter(runner -> runningTime > runner.minMinutes && runningTime <= runner.maxMinutes)
+                .findFirst()
+                //.orElse(BEGINNER);
+                .orElseThrow(() -> new IllegalArgumentException("Podany czas jest spoza możliwych przedziałów"));
+    }
+}
---
+++ src/pl/sdacademy/java/advanced/exercises/day2/task16/Task16.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.java.advanced.exercises.day2.task16;
+
+public class Task16 {
+    public static void main(String[] args) {
+        Runner fitnessLevel = Runner.getFitnessLevel(-1);
+        System.out.println(fitnessLevel.name());
+    }
+}
